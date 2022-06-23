8e86c4f80b53a316f4c1cfd33bc270002abe780b
alissonnxavier <alissonalves29@gmail.com>
2022-06-23 10:13:48 -0300

    adicionado redimensionamento para imagens

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Produto
+
 
 # Register your models here.
+
+
+
+
+admin.site.register(Produto)
--- produto/models.py
+++ produto/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from PIL import Image
+import os
+from django.conf import settings
 
 # Create your models here.
 
@@ -14,4 +17,39 @@ class Produto(models.Model):
     preco_marketing_promocional = models.FloatField(default=0)
     tipo = models.CharField(default='V', max_length=1, choices=(('V', 'Variação'), ('S', 'Simples')))
 
+    @staticmethod
+    def resize_image(img, new_width=800):
+        img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+        img_pil = Image.open(img_full_path)
+        original_width, original_height = img_pil.size
+        print(img.name)
 
+        width2 = original_width
+        height2 = original_height
+
+        if original_height > original_width:
+            original_width = original_height
+            original_height = width2
+
+        if original_width <= new_width:
+            print('Retornando, largura original menor que nova largura')
+            img_pil.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+
+        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(img_full_path, optimize=True, quality=50)
+        print('Imagem foi redimensionada')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        max_image_size = 800
+
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
+
+
+    def __str__(self):
+        return self.nome
